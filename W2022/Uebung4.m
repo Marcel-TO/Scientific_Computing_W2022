0c8c605930f8778f33e942a87987eefbe5959e68
Turobin-Ort Marcel <116675@fhwn.ac.at>
2022-11-02 11:36:10 +0100

    Updated 4th homework.

--- W2022/Uebung4.m
+++ W2022/Uebung4.m
@@ -1,2 +1,20 @@
 %%
-% Aufgabe 2 Poissrnd Nieren -> Siehe W2021/Uebung4.m
+% Aufgabe 1
+
+%%
+% Aufgabe 2
+
+%%
+% Aufgabe 3
+
+%%
+% Aufgabe 4 Poissrnd Nieren -> Siehe W2021/Uebung4.m
+% a)
+n=1000;
+ma = max(exprnd(1,1,n), exprnd(2,1,n));
+pa = sum(ma) / n;
+
+%b)
+n=1000;
+ma = max(exprnd(1,1,n), exprnd(2,1,n));
+pb = sum(ma) / n;
